3390d738e793719cc7b5c2fddd62935dfefcc403
smallai <hongjie_ai@163.com>
2019-01-24 12:12:48 +0000

    Updated database configuration

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hongjie
- * Date: 19-1-24
- * Time: 下午8:10
- */
+
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
