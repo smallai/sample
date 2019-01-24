885ed8d9a797893bb57c8aacd7ea7a6440969ebb
smallai <hongjie_ai@163.com>
2019-01-24 12:21:33 +0000

    fix error notice

--- resources/views/shared/_errors.blade.php
+++ resources/views/shared/_errors.blade.php
@@ -1,9 +1,9 @@
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+@foreach (['danger', 'warning', 'success', 'info'] as $msg)
+    @if(session()->has($msg))
+        <div class="flash-message">
+            <p class="alert alert-{{ $msg }}">
+                {{ session()->get($msg) }}
+            </p>
+        </div>
+    @endif
+@endforeach
