15e0a7fab3e0b9e829fd2cad92787df646cbc254
smallai <hongjie_ai@163.com>
2019-01-24 10:36:46 +0000

    finished layouts and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,17 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Sample')</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    @yield('content')
+
+    @include('layouts._header')
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    @include('layouts._footer')
+
 </body>
 </html>
